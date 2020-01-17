b10ca724e64e7920e793b77f36b7b147c22d65ba
royc-bb <rc1@hesperides.ca>
2020-01-16 20:09:34 -0500

    Update exception for the database service
    added serialVersionUID to support serialization

--- src/main/java/com/example/demo/persistence/RecordNotFoundException.java
+++ src/main/java/com/example/demo/persistence/RecordNotFoundException.java
@@ -1,6 +1,9 @@
 package com.example.demo.persistence;
 
 public class RecordNotFoundException extends Exception {
+
+  private static final long serialVersionUID = -2789559542028129297L;
+
   public RecordNotFoundException(String msg) {
     super(msg);
   }
